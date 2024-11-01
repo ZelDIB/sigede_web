94144be80a7dc3d717d0fd36be78ded3d7a739a5
EmilioVejarDiaz <20213tn091@utez.edu.mx>
2024-10-31 21:14:54 -0600

    [ADD] feature/vista_QR - Visualización de datos: Se creo la base de los componentes, el navbar y la vista del QR
    Hace falta pulir el LOGO y el color del navbar ya que no es un color totalmente obscuro.

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
+    <Navbar />
     <NuxtPage />
   </div>
 </template>
+
+<script setup>
+import Navbar from "./components/NavBar.vue";
+</script>
+
+<style>
+body {
+  background-color: #E4E4E4;
+  margin: 0;
+  height: 100vh;
+}
+</style>
---
+++ components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <NuxtLink to="/" class="navbar-item" style="margin-left: 10px;">
+          <img src="../assets/logo/LOGO.png" alt="SIGEDE Logo" height="40">
+        </NuxtLink>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: "NavBar",
+  };
+  </script>
+  
+  <style scoped>
+  .navbar-item img {
+    height: 40px;
+  }
+  .navbar-item{
+    background-color: black;
+  }
+  </style>
+  
