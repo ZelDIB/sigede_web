d407d1dc0a789f3168a18972b6c1a8f1665972e8
OmarVerdayes <20213tn042@utez.edu.mx>
2024-11-29 00:58:01 -0600

    consult cloudinary

---
+++ pages/admins/screens/RegisterImage.vue
@@ -0,0 +1,18 @@
+<template>
+    <NuxtImg 
+    provider="cloudinary" 
+    src="https://res.cloudinary.com/dpkl7ms3o/image/upload/v1732779300/docs/models.gif"
+    width="200" 
+    height="200"
+    :modifiers="{roundCorner:'max'}" 
+    />
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style></style>
