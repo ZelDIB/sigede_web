d7e91fae2c8d302d3ef6ae426623b44620658b4e
FREECS33 <20213tn116@utez.edu.mx>
2024-12-10 10:00:32 -0600

    [UPDATE] preparing all for deploy with jenkins

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const axiosInstance = axios.create({
-    baseURL: process.env.API_BASE_URL || "http://localhost:8080/",
+    baseURL: process.env.API_BASE_URL,
   });
 
   const { $toast } = useNuxtApp();
