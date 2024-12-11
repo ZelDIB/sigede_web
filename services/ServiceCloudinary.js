34ef4007846bb2fd016a7063b8bd9aab774f212e
Alejandro Ibarra Brito <105244322+FREECS33@users.noreply.github.com>
2024-12-11 16:14:55 -0600

    Update ServiceCloudinary.js

--- services/ServiceCloudinary.js
+++ services/ServiceCloudinary.js
@@ -5,10 +5,18 @@ export const ServiceCloudinary = {
         const formData = new FormData();
         formData.append("file", file);
         formData.append("upload_preset", "dpkl7ms3o-sigede"); 
+
         try {
+            // Hacemos una copia de las cabeceras actuales, pero sin el Authorization
+            const headers = { ...axios.defaults.headers };
+
+            // Eliminamos el Authorization en esta solicitud específica
+            delete headers["Authorization"];
+
             const response = await axios.post(
                 "https://api.cloudinary.com/v1_1/dpkl7ms3o/image/upload",
-                formData
+                formData,
+                { headers } // Pasamos las cabeceras sin el Authorization
             );
             return response.data.secure_url; 
         } catch (error) {
