f155c31b76b056d70fe05e1cc0d84c89740b4324
FREECS33 <20213tn116@utez.edu.mx>
2024-12-12 10:06:01 -0600

    [UPDATE] navbar for superadmin and some details

--- utils/navbarConfig.js
+++ utils/navbarConfig.js
@@ -1,17 +1,41 @@
 export const navbarConfig = {
   ADMIN: [
-    { icon: "fa-brands fa-wpforms", route: "/admins/screens/ClientForm" },
     {
+      name: "Formulario",
+      icon: "fa-brands fa-wpforms",
+      route: "/admins/screens/ClientForm",
+    },
+    {
+      name: "Plantilla",
       icon: "fa-regular fa-file-word",
       route: "/admins/screens/ManageDocument",
     },
-    { icon: "fas fa-users", route: "/admins/screens/CapturistList" },
-    { icon: "fas fa-user", route: "/auth/Profile" },
+    {
+      name: "Capturistas",
+      icon: "fas fa-users",
+      route: "/admins/screens/CapturistList",
+    },
+    { name: "Perfil", icon: "fas fa-user", route: "/auth/Profile" },
   ],
-  SUPERADMIN: [{ name: "Home", icon: "fas fa-home", route: "/" }],
+  SUPERADMIN: [
+    {
+      name: "Organizaciones",
+      icon: "fa-solid fa-building",
+      route: "/superdadmins/screens/OrganizationsList",
+    },
+    {
+      name: "Perfil",
+      icon: "fas fa-user",
+      route: "/auth/Profile",
+    },
+  ],
   CAPTURISTA: [
-    { icon: "fas fa-users", route: "/capturists/screens/CredentialsList" },
-    { icon: "fas fa-user", route: "/auth/Profile" },
+    {
+      name: "Credenciales",
+      icon: "fas fa-users",
+      route: "/capturists/screens/CredentialsList",
+    },
+    { name: "Perfil", icon: "fas fa-user", route: "/auth/Profile" },
   ],
   DEFAULT: [{ name: "Home", icon: "fas fa-home", route: "/" }],
 };
