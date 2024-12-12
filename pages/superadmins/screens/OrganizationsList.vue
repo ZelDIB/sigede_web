214ae341df7acc28cb3e03c97a825264bca68c36
Yexzari <20203tn141@utez.edu.mx>
2024-12-12 15:02:59 -0600

    Canges presentation

--- pages/superadmins/screens/OrganizationsList.vue
+++ pages/superadmins/screens/OrganizationsList.vue
@@ -228,7 +228,7 @@ body {
 
 .grid-container {
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-template-columns: repeat(4, 1fr);
   gap: 15px;
   margin-top: 20px;
   justify-content: center;
